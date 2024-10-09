14baf3f2063995735ade962ded809eacefdf7d2e
rifkyariy <rifkyariy@gmail.com>
2024-10-09 10:20:09 +0800

    change: using webxr

--- src/components/main/LockScreen.tsx
+++ src/components/main/LockScreen.tsx
@@ -4,14 +4,19 @@ import { useState } from 'react'
 
 const store = createXRStore()
 
-export default function App() {
-  const [red, setRed] = useState(false)
+const App: React.FC = () => {
+  const [red, setRed] = useState<boolean>(false)
+
   return (
     <>
       <button onClick={() => store.enterAR()}>Enter AR</button>
       <Canvas>
         <XR store={store}>
-          <mesh pointerEventsType={{ deny: 'grab' }} onClick={() => setRed(!red)} position={[0, 1, -1]}>
+          <mesh
+            pointerEventsType={{ deny: 'grab' }} // TypeScript might not have built-in types for this; may need to use `any` or custom type if needed
+            onClick={() => setRed(!red)}
+            position={[0, 1, -1]}
+          >
             <boxGeometry />
             <meshBasicMaterial color={red ? 'red' : 'blue'} />
           </mesh>
@@ -20,3 +25,5 @@ export default function App() {
     </>
   )
 }
+
+export default App
